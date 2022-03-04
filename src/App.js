2c71e4135e59585d0a56b99cb5cdd7a6d34876ba
BrandonGreenOAB <brandon.green@knights.ucf.edu>
2022-03-03 21:54:46 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import HoneyBearsMenu from "./components/HoneyBearsMenu";
+
+function App() {
+  return (
+    <div className="App">
+      <HoneyBearsMenu />
+    </div>
+  );
+}
+
+export default App;
